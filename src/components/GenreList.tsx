1f885d9572347a11f28df59ada16a3de72460e10
hburaks <hasanburaksongur@gmail.com>
2023-04-18 13:17:00 +0300

    Show aoading skeletons while fetching genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,22 @@
 import { HStack, List, ListItem, Image, Text, Spinner } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
+import { v4 as uuidv4 } from "uuid";
+import ListItemSkeleton from "./ListItemSkeleton";
 
 const GenreList = () => {
 	const { data, isLoading, error } = useGenres();
-	if (isLoading) return <Spinner />;
+	const skeletons = [1, 2, 3, 4, 5, 6];
+
 	if (error) return null;
 	return (
 		<List>
+			{isLoading &&
+				skeletons.map(() => (
+					<ListItem key={uuidv4()}>
+						<ListItemSkeleton />
+					</ListItem>
+				))}
 			{data.map((genre) => (
 				<ListItem key={genre.id} paddingY="5px">
 					<HStack>
@@ -26,3 +35,28 @@ const GenreList = () => {
 };
 
 export default GenreList;
+
+// const { data, error, isLoading } = useGames();
+// 	const skeletons = [1, 2, 3, 4, 5, 6];
+// 	return (
+// 		<>
+// 			{error && <Text>{error}</Text>}
+// 			<SimpleGrid
+// 				columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+// 				spacing={3}
+// 				padding="10px"
+// 			>
+// 				{isLoading &&
+// 					skeletons.map(() => (
+// 						<GameCardContainer key={uuidv4()}>
+// 							<GameCardSkeleton />
+// 						</GameCardContainer>
+// 					))}
+// 				{data.map((game) => (
+// 					<GameCardContainer key={uuidv4()}>
+// 						<GameCard key={game.id} game={game} />
+// 					</GameCardContainer>
+// 				))}
+// 			</SimpleGrid>
+// 		</>
+// 	);
---
+++ src/components/ListItemSkeleton.tsx
@@ -0,0 +1,11 @@
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+
+const ListItemSkeleton = () => {
+	return (
+		<Card padding={5} marginY={5}>
+			<SkeletonText height={10} />
+		</Card>
+	);
+};
+
+export default ListItemSkeleton;
